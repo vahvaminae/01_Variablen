b8d88159a81afcab0abfeaa432d4b313c731c0be
Jenny <JenifferLeutert@gmx.de>
2023-07-13 11:00:57 +0200

    Unterschiedliche Typsierungen

--- script_01.js
+++ script_01.js
@@ -14,11 +14,32 @@
 
 
 /***** 02 Deklaration + Wertzuweisung I *******/
-let firstName; // Deklaration (Definition) //let teilt dem Compiler mit, wenn man die Bezeichnung
+// let firstName; // Deklaration (Definition) //let teilt dem Compiler mit, wenn man die Bezeichnung
 //firstname verwendet, dann zeigt er an, dass man nur diesen Speicherbereich meint
-firstName = "Max"; //Wertzuweisung
-console.log(firstName); // Ausgabe
+// firstName = "Max"; //Wertzuweisung
+// console.log(firstName); // Ausgabe
 
-let familyName;
-familyName = "Hauser";
-console.log(familyName);
+// let familyName;
+// familyName = "Hauser";
+// console.log(familyName);
+
+/****03 Deklarationen & Wertzuweisungen II */
+
+// let firstName, familyName;
+
+// firstName = prompt("Bitte Vornamen eingeben:");
+// familyName = prompt("Bitte Nachnamen eingeben:");
+
+// console.log(firstName,familyName); 
+// console.log(firstName + " " + familyName); 
+
+/* JS ist eine untypisierte Sprache! | untyped */
+let test;
+test ="Maxime";
+test =2;
+test = false;
+
+console.log("Datentypen: "+ typeof test);
+console.log("Inhalt: " + test);
+
+
