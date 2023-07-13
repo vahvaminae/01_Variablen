c34dbd67dbb1443a4f41944664bf2308b4da484e
Jenny <JenifferLeutert@gmx.de>
2023-07-13 12:08:02 +0200

    Skizze Jahreszahl

--- script_01.js
+++ script_01.js
@@ -53,13 +53,19 @@
 
 //Deklaration
 
-let ageJohn, ageMarc;
-const birthYearJohn = 2000;
-const birthYearMarc = 1990;
+// let ageJohn, ageMarc;
+// const birthYearJohn = 2000;
+// const birthYearMarc = 1990;
 
-let year = 2023;
-ageJohn = year - birthYearJohn
-ageMarc = year - birthYearMarc
+// let year = 2023;
+// ageJohn = year - birthYearJohn
+// ageMarc = year - birthYearMarc
 
-console.log("ageJohn: " + ageJohn)
-console.log("ageMarc: " + ageMarc)
+// console.log("ageJohn: " + ageJohn)
+// console.log("ageMarc: " + ageMarc)
+
+let date = new Date();
+let year = date.getFullYear();
+// console.log(date);
+console.log(year);
+
