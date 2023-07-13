0a2a17b5232c1a0f67c3f87e5e13cd7c151b391e
Jenny <JenifferLeutert@gmx.de>
2023-07-13 10:17:50 +0200

    Deklarationen und Wertzuweisungen

--- script_01.js
+++ script_01.js
@@ -9,9 +9,16 @@
 */
 
 // Konsolen-Ausgaben
-console.log("Hi");
+//console.log("Hi");
 // console.log(firstName);
 
 
 /***** 02 Deklaration + Wertzuweisung I *******/
+let firstName; // Deklaration (Definition) //let teilt dem Compiler mit, wenn man die Bezeichnung
+//firstname verwendet, dann zeigt er an, dass man nur diesen Speicherbereich meint
+firstName = "Max"; //Wertzuweisung
+console.log(firstName); // Ausgabe
 
+let familyName;
+familyName = "Hauser";
+console.log(familyName);
